194f86fbf50b430632e1f53326cc8064eaa5133f
RevanWang <zjqx1991@163.com>
2019-06-19 20:30:39 +0800

    屏幕适配flutter_screenutil

--- lib/classes/home/home_page.dart
+++ lib/classes/home/home_page.dart
@@ -4,6 +4,7 @@ import '../http/shop_service.dart';
 import '../http/revan_response.dart';
 import 'model/slides_model.dart';
 import 'package:flutter_page_indicator/flutter_page_indicator.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 
 class HomePage extends StatefulWidget {
@@ -32,6 +33,9 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
 
+    ScreenUtil.instance = ScreenUtil(width: 750, height: 1334)..init(context);
+
+
     if (this.model == null) {
       return Scaffold(
           appBar: AppBar(
@@ -55,16 +59,25 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
       body: Container(
-        color: Colors.red,
-        child: Swiper(
-          itemCount: this.model.slides.length,
-          itemBuilder: (BuildContext context, int index) {
-              return Image.network(
-                this.model.slides[index].image
-              );
-          },
-          autoplay: true,
-          indicatorLayout: PageIndicatorLayout.NONE,
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: ScreenUtil.getInstance().setHeight(333.0),
+              child: Swiper(
+                itemCount: this.model.slides.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Image.network(
+                      this.model.slides[index].image,
+                    fit: BoxFit.fill,
+                  );
+                },
+                autoplay: true,
+                indicatorLayout: PageIndicatorLayout.NONE,
+                pagination: SwiperPagination(),
+
+              ),
+            )
+          ],
         ),
       )
     );
